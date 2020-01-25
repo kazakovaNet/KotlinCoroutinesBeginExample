acf977bf31bfd24cb6738c87973063e03239948f
kazakovaNet <kazakova.net@yandex.ru>
2020-01-25 17:40:33 +0300

    Fake request api

--- app/src/main/java/ru/kazakova_net/kotlincoroutinesbeginexample/MainActivity.kt
+++ app/src/main/java/ru/kazakova_net/kotlincoroutinesbeginexample/MainActivity.kt
@@ -1,12 +1,66 @@
 package ru.kazakova_net.kotlincoroutinesbeginexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "Result #1"
+    private val RESULT_2 = "Result #2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            // Dispatchers.Main, Dispatchers.IO, Dispatchers.Default
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
+        }
+    }
+
+    private fun setNewText(input: String) {
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
+    private suspend fun fakeApiRequest() {
+        val result1 = getResult1FromApi()
+        println("debug: $result1")
+        setTextOnMainThread(result1)
+
+        val result2 = getResult2FromApi(result1)
+        println("debug: $result2")
+        setTextOnMainThread(result2)
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(result1: String): String {
+        logThread("getResult2FromApi")
+        delay(1000)
+        return "$result1 -> $RESULT_2"
+    }
+
+    private fun logThread(methodName: String) {
+        println("debug: $methodName: ${Thread.currentThread().name}")
     }
 }
